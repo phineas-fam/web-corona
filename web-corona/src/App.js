cb6cf8f72ddc5027976db5675deb3e4b2dfdfbb5
Mulayo <53578009+Mulayo@users.noreply.github.com>
2020-04-15 18:52:58 +0200

    Update App.js

--- web-corona/src/App.js
+++ web-corona/src/App.js
@@ -6,7 +6,9 @@ class Form extends React.Component {
     super(props);
     this.state = {
       squares: {},
-      listt: []
+      listt: [{"id":0,"text":"Have you come into close contact with someone who has tested positive for COVID19 ?"},{"id":1,"text":"Do you have difficulty when breathing?"},{"id":2,"text":"Do you have fever or a dry cough ?"}],
+      showNext:[true],
+      index : 1
     };
   }
 
@@ -14,22 +16,47 @@ class Form extends React.Component {
     const squares2 = this.state.squares;
     const question = Event.target.name;
     const answer = Event.target.value;
+    const showElemnts = this.state.showNext;
+    //  showElemnts[0] will always be true 
+    showElemnts[0]=true; 
+    showElemnts[this.state.index]=true;
+    const new_index =this.state.index + 1;
+    console.log(new_index);
     squares2[question] = { question: question, text: answer };
     this.setState({
-      squares: squares2
+      squares: squares2,
+      showNext:showElemnts,
+      index : new_index
     });
   };
 
-  componentDidMount() {
-    fetch("http://localhost:5000/questions/")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ listt: data });
-      });
+  // componentDidMount() {
+  //   fetch("http://localhost:5000/questions/")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log("what the ");
+  //       this.setState({ listt: data });
+  //       //console.log(this.state.squares2)
+       
+  //     });
+  //     //console.log(this.state.squares)
+     
+  // }
+
+  chanh=()=>{
+    const no =  this.state.no + 1
+    console.log(no)
+    this.setState({
+      no: 2
+    });
+
   }
 
   render() {
+
+    //console.log("man ",this.state.listt);
     return (
+      
       <div className="App">
         <header className="App-header">
           <img
@@ -43,14 +70,19 @@ class Form extends React.Component {
               <b> Covid-19 </b>
             </div>
             <div style={{ fontSize: "30px" }}>
-              <b> Online Survey</b>
+              <b> Online Survey </b>
             </div>
           </div>
+
         </header>
 
         <form>
+
+
+
           {this.state.listt.map(question => (
-            <div key={question.id}>
+            // currently i used the id to hide but we'll  use the index in future
+            <div key={question.id} className={this.state.showNext[question.id]===true ? "show" :"hide"}>
               <h2>
                 <b>{question["text"]}</b>
               </h2>
@@ -58,7 +90,7 @@ class Form extends React.Component {
               <input
                 id={`${question["id"]}_Yes`}
                 className="radio-custom"
-                name={question["id"]}
+                name={"azwindini"}
                 type="radio"
                 value="Yes"
                 onClick={() => {
@@ -94,8 +126,7 @@ class Form extends React.Component {
               </label>
             </div>
           ))}
-        </form>
-        <div>
+                  <div>
           <button
             className="btn"
             onClick={() => {
@@ -106,6 +137,8 @@ class Form extends React.Component {
             Submit{" "}
           </button>
         </div>
+        </form>
+
       </div>
     );
   }
@@ -121,3 +154,6 @@ const onSubmit = answersArray => {
   });
 };
 export default Form;
+
+ 
+
